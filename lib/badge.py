815160187511e25451adbe012602fa24aeadc181
roomthily <sorenscott@gmail.com>
2015-09-04 09:46:41 -0600

    new badge class
    supports the three default shields.io styles (flat, flat rounded,
    plastic)

--- lib/badge.py
+++ lib/badge.py
@@ -17,14 +17,23 @@ class Badge():
             "background": "#eee",
             "href": "http://www.github.com"
         }
+
+    supports shields.io plastic, flat round and flat square styles
+    and href links for left and right.
     '''
+
+    # default sizing
     _pad = 8
     _sep = 4
     _height = 20
 
-    def __init__(self, left, right):
+    def __init__(self, left, right, style="flat"):
         self.left = left
         self.right = right
+        self.style = style
+
+        if style == 'plastic':
+            self._height = 18
 
     def _text_width(self, text):
         return 7 * len(text)
@@ -38,64 +47,48 @@ class Badge():
             self._sep
         return left_width, right_width, left_width + right_width
 
-    def _generate_text(self, text, x, y, href='', fill=''):
-        # return the two text svg elements, text and shadow
-        # x and y as strings
-
-        # TODO: sort out which of these should have the href (and
-        # is it the mask that's the issue?)
-        svg_text = svgwrite.text.Text(
-            text=text,
-            x=[str(x)],
-            y=[str(y)],
-            **{"fill": "#010101", "fill-opacity": ".3"}
+    # svg fragments
+    def _generate_linear_gradient(self):
+        lg = svgwrite.gradients.LinearGradient(
+            end=(0, "100%"),
+            **{"id": "b"}
         )
-        # currently doing bupkus
-#         if href:
-#             svg_text.add(
-#                 svgwrite.container.Hyperlink(href=href, target='_blank'))
+        if self.style == 'flat round':
+            lg.add_stop_color(offset=0, color="#bbb", opacity=".1")
+            lg.add_stop_color(offset=1, opacity=".1")
+        elif self.style == 'plastic':
+            lg.add_stop_color(offset=0, color="#fff", opacity=".7")
+            lg.add_stop_color(offset=0.1, color="#aaa", opacity=".1")
+            lg.add_stop_color(offset=0.9, opacity=".3")
+            lg.add_stop_color(offset=1, opacity=".5")
+        return lg
 
-        yield svg_text
-
-        extras = {"fill": fill} if fill else {}
-        yield svgwrite.text.Text(
-            text=text,
-            x=[str(x)],
-            y=[str(y - 1)],
-            **extras
-        )
-
-    def generate_badge(self):
-        lw, rw, tw = self._calculate_widths()
-
-        # build the svg
-        svg = svgwrite.container.SVG(
-            size=(tw, 20),
-            **{
-                "xmlns": "http://www.w3.org/2000/svg",
-                "xmlns:xlink": "http://www.w3.org/1999/xlink"
-            }
-        )
-
-        # add the linear gradient
-        linear_gradient = svgwrite.gradients.LinearGradient(
-            end=(0, "100%"), **{"id": "b"}
-        )
-        linear_gradient.add_stop_color(offset=0, color="#bbb", opacity=".1")
-        linear_gradient.add_stop_color(offset=1, opacity=".1")
-        svg.add(linear_gradient)
-
-        # add the mask
+    def _generate_mask(self, tw):
         mask = svgwrite.masking.Mask(**{"id": "a"})
         mask_rect = svgwrite.shapes.Rect(
-            size=(tw, 20),
-            rx=3,
+            size=(tw, self._height),
+            rx=4 if self.style == 'plastic' else 3,
             **{"fill": "#fff"}
         )
         mask.add(mask_rect)
-        svg.add(mask)
+        return mask
 
-        # and the group, with mask
+    def _generate_background(self, lw, rw, tw):
+        if self.style == 'flat square':
+            group = svgwrite.container.Group(
+                **{"shape-rendering": "crispEdges"})
+            group.add(svgwrite.shapes.Rect(
+                size=(lw, self._height),
+                **{"fill": self.left.get('background')})
+            )
+            group.add(svgwrite.shapes.Rect(
+                size=(rw, self._height),
+                insert=(lw, 0),
+                **{"fill": self.right.get('background')})
+            )
+            return group
+
+        # and the other two
         group = svgwrite.container.Group(**{"mask": "url(#a)"})
         group.add(svgwrite.shapes.Rect(
             size=(lw, self._height),
@@ -112,28 +105,110 @@ class Badge():
                 **{"fill": "url(#b)"}
             )
         )
-        svg.add(group)
+        return group
 
-        # add the text group
+    def _generate_text_group(self, lw, rw):
+        '''
+        NOTE: the y values are taken from the shields.io templates
+        '''
         tg = {
             "fill": "#fff",
             "text-anchor": "middle",
             "font-family": "DejaVu Sans,Verdana,Geneva,sans-serif",
             "font-size": "11"
         }
-        text_group = svgwrite.container.Group(**tg)
-        for t in self._generate_text(
-                self.left.get('text'),
-                (lw / 2) + 1, 15,
-                href=self.left.get('href', '')
-        ):
-            text_group.add(t)
-        for t in self._generate_text(
-            self.right.get('text'),
-            lw + ((rw / 2) - 1),
-            15
-        ):
-            text_group.add(t)
-        svg.add(text_group)
+        group = svgwrite.container.Group(**tg)
+
+        if self.style == 'flat square':
+            group.add(svgwrite.text.Text(
+                text=self.left.get('text'),
+                x=[str((lw / 2) + 1)],
+                y=['14']
+            ))
+            group.add(svgwrite.text.Text(
+                text=self.right.get('text'),
+                x=[str(lw + (rw / 2) - 1)],
+                y=['14']
+            ))
+        elif self.style == 'flat round':
+            # left
+            lx = (lw / 2)  # + 1
+            group.add(svgwrite.text.Text(
+                text=self.left.get('text'),
+                x=[str(lx)],
+                y=['15'],
+                **{"fill": "#010101", "fill-opacity": ".3"}
+            ))
+            group.add(svgwrite.text.Text(
+                text=self.left.get('text'),
+                x=[str(lx)],
+                y=['14']
+            ))
+
+            # right
+            lx = lw + (rw / 2) - 1
+            group.add(svgwrite.text.Text(
+                text=self.right.get('text'),
+                x=[str(lx)],
+                y=['15'],
+                **{"fill": "#010101", "fill-opacity": ".3"}
+            ))
+            group.add(svgwrite.text.Text(
+                text=self.right.get('text'),
+                x=[str(lx)],
+                y=['14']
+            ))
+        elif self.style == 'plastic':
+            # left
+            lx = (lw / 2) + 1
+            group.add(svgwrite.text.Text(
+                text=self.left.get('text'),
+                x=[str(lx)],
+                y=['14'],
+                **{"fill": "#010101", "fill-opacity": ".3"}
+            ))
+            group.add(svgwrite.text.Text(
+                text=self.left.get('text'),
+                x=[str(lx)],
+                y=['13']
+            ))
+
+            # right
+            lx = lw + (rw / 2) - 1
+            group.add(svgwrite.text.Text(
+                text=self.right.get('text'),
+                x=[str(lx)],
+                y=['14'],
+                **{"fill": "#010101", "fill-opacity": ".3"}
+            ))
+            group.add(svgwrite.text.Text(
+                text=self.right.get('text'),
+                x=[str(lx)],
+                y=['13']
+            ))
+
+        return group
+
+    def generate_badge(self):
+        # now with styles
+        lw, rw, tw = self._calculate_widths()
+
+        # build the svg
+        extras = {"xmlns": "http://www.w3.org/2000/svg"}
+        if 'href' in self.left or 'href' in self.right:
+            extras["xmlns:xlink"] = "http://www.w3.org/1999/xlink"
+        svg = svgwrite.container.SVG(
+            size=(tw, self._height),
+            **extras
+        )
+
+        if self.style in ['flat round', 'plastic']:
+            svg.add(self._generate_linear_gradient())
+
+            svg.add(self._generate_mask(tw))
+
+        svg.add(self._generate_background(lw, rw, tw))
+
+        svg.add(self._generate_text_group(lw, rw))
 
         return svg.tostring()
